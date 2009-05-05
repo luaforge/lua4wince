48e3e1f2da140c18d395453818badc5108ea7b3e
Luis Eduardo Jason Santos <jasonsantos@luaforge.net>
2009-05-05 14:55:48 +0000

    Cleanup of the xavantetray module

--- org.keplerproject.lua4wince/source/xavantetray/resource.h
+++ org.keplerproject.lua4wince/source/xavantetray/resource.h
@@ -30,14 +30,3 @@
 #define ID_APP_ABOUT                    0xE140
 #define ID_APP_EXIT                     0xE141
 #define IDC_STATIC                      -1
-
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        135
-#define _APS_NEXT_COMMAND_VALUE         32776
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           110
-#endif
-#endif
